094ff21177ea6bc9da7a8cf58b2ed1a93a38a2ac
wangqi <wangqi@xinxuan.net>
2025-07-10 17:41:46 +0800

    fix: workflow error

--- build/mvn-build.sh
+++ build/mvn-build.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 echo 'taier Building...'
 
-mvn clean package -DskipTests -T 1C -pl \
-taier-ui,\
-taier-data-develop,\
-taier-common,\
-taier-worker/taier-worker-plugin,\
-taier-datasource/taier-datasource-plugin \
--am -amd
+mvn clean package -DskipTests -T 1C -pl taier-ui,taier-data-develop,taier-common,taier-worker/taier-worker-plugin,taier-datasource/taier-datasource-plugin -am -amd
